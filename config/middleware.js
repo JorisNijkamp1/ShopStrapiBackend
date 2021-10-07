cbc0bd1f73730fe3c585ed3501b6ad42351a4e10
Joris Nijkamp <JGW.Nijkamp@student.han.nl>
2021-10-07 17:51:53 +0200

    CORS

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,8 @@
-module.exports = ({env}) => ({
+module.exports = () => ({
   settings: {
     cors: {
       enabled: true,
-      origin: env('CORS_ORIGIN', 'http://localhost:3000,https://shop.jorisnijkamp.nl,http://localhost:1337').split(",")
+      origin: ['*']
     }
   }
 })
